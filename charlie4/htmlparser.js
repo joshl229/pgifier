55117f2c3a857abdc294e93b28f00d0f3301226d
Drew Litkowiak <dnl2@illinois.edu>
2019-02-23 00:03:20 -0600

    added a script to the html file

---
+++ charlie4/htmlparser.js
@@ -0,0 +1,9 @@
+var corpus;
+
+
+function main() {
+  console.log("Executing main");
+  frame = document.getElementById('nested_webpage');
+  corpus = frame.getElementsByTagName('BODY')[0];
+  console.log("Finished Executing main");
+}
